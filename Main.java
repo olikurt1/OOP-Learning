28760f6d79ba756d9aa96dd1cf540678bd858db0
olikurt1 <olikurt1@outlook.com>
2026-04-14 16:16:17 +0100

    Attempting to abstract the classes so that regardless of attack method type they can both override the hero default one

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main{
     public static void main(String[] args){
         //creates an object of avengers type, this essentially provides a funnel into the class using the ironMan identifier
         hero ironMan = new avengers("Tony Stark", 100);
-        viltrumite vil1 = new viltrumite("OmniMan", 1000);
+        hero vil1 = new viltrumite("OmniMan", 1000, ironMan);
         
         //main can no longer see ironMan's name straight from the class so instead the getter function had to be used to retrieve the name from the object
         System.out.println("This hero's name is "+ironMan.getName());
@@ -12,19 +12,20 @@ class Main{
         ironMan.attack();
 
         
-        int ironManDamage = vil1.headChop(ironMan);
+        int ironManDamage = vil1.attack(ironMan);
         ironMan.setPower(ironManDamage);
         System.out.println("OmniMan attacked, IronMan's power is now "+ironMan.getPower()+".");
         String ironManStatus = (ironMan.getPower() <= 0) ? "Iron Man is dead" : "Iron Man is still alive";
         System.out.println(ironManStatus);
-        vil1.attack();
+        
     }
 }
-class hero{
+abstract class hero{
     //defines the attributes a hero may have, use the protected keyword because it modifies access by only allowing access 
     // to them by hero itself or classes that inherit hero
     protected String name;
     protected int power;
+    protected hero hero;
     //class constructor means that attribute values can be directly passed to object rather than hardcoded to main
     //allowing encapsulation
     hero(String name, int power){
@@ -44,9 +45,7 @@ class hero{
     public int getPower(){
         return power;
     }
-    void attack(){
-        System.out.println("Generic attack");
-    }
+    abstract int attack();   
 }
 
 class avengers extends hero{
@@ -58,18 +57,24 @@ class avengers extends hero{
     //uses the hero attack method but overrides it with its own, this way, can be initialised as a hero type but an avenger object 
     //this is a use case of polymorphism where the parent/hero class steps in for the child/avengers or viltrumite class
     @Override //this keyword strictly tells the program that the method is overriding its original in the parent class
-    void attack(){
+    int attack(){
         System.out.println("Ultron takes "+power+" damage.");
         System.out.println("Thanos only takes "+power/2+" damage because he's hench.");
+        return 0;
     }
 }
 
 class viltrumite extends hero{
-    viltrumite(String name, int power){
+    viltrumite(String name, int power, hero hero){
         super(name, power);
+        this.hero = hero;
+    }
+    public void setTargetToAttack(hero h){
+        this.hero = h;
     }
     //dont use static because otherwise it cannot use its own objects values/ it cannot have "this.power"
-    int headChop(hero h){
+    @Override
+    int attack(hero h){
         int heroPower = h.getPower();
         heroPower -= this.power;
         if(heroPower < 0){
@@ -77,9 +82,5 @@ class viltrumite extends hero{
         }
         return heroPower;
         
-    }
-    //takes the hero attack method and overrides it with its own version
-    void attack(){
-        System.out.println(this.name+"'s enemy takes "+this.power/2+ " damage.");
     }
 }
