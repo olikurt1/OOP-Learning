0d81304bd82f3f7f0e128e87f11c73a494147bce
olikurt1 <olikurt1@outlook.com>
2026-04-14 10:24:18 +0100

    Providing the classes with constructors

--- Main.java
+++ Main.java
@@ -3,17 +3,14 @@ package OOPtime;
 class Main{
     public static void main(String[] args){
         //creates an object of avengers type, this essentially provides a funnel into the class using the ironMan identifier
-        avengers ironMan = new avengers();
+        avengers ironMan = new avengers("Tony Stark", 100);
         //ironMan accesses the class through the intialiser above and .name specifies that the name attribute is being characterised
-        ironMan.name = "Tony Stark";
-        ironMan.power = 100;
+        
         System.out.println("This hero's name is "+ironMan.name);
         //calling the method within avengers 
         ironMan.punch();
 
-        viltrumite vil1 = new viltrumite();
-        vil1.name = "OmniMan";
-        vil1.power = 1000;
+        viltrumite vil1 = new viltrumite("OmniMan", 1000);
         int ironManDamage = vil1.headChop(ironMan);
         ironMan.power = ironManDamage;
         System.out.println("OmniMan attacked, IronMan's power is now "+ironMan.power+".");
@@ -26,6 +23,13 @@ class avengers{
     //define the attributes that an avenger may have, this is also known as constructing the class
     String name;
     int power;
+    //providing a constructor means that attributes can be passed directly to the class instance rather 
+    // than being hard coded in main and exposed
+    avengers(String name, int power){
+        this.name = name;
+        this.power = power;
+    }
+    
     void punch(){
         System.out.println("Ultron takes "+power+" damage or some shit.");
         System.out.println("Thanos only takes "+power/2+" damage because he's hench.");
@@ -35,6 +39,12 @@ class avengers{
 class viltrumite{
     String name;
     int power;
+    
+    viltrumite(String name, int power){
+        this.name = name;
+        this.power = power;
+    }
+
     //dont use static because otherwise it cannot use its own objects values/ it cannot have "this.power"
     int headChop(avengers a){
         int AvengerPower = a.power;
